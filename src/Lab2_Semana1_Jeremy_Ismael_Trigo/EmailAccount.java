062e5555dda843918be0ac8c084865053ec6e9d4
GioMej14 <trigoamaya1@gmail.com>
2025-10-17 10:16:16 -0600

    Creacion EmailAccount

---
+++ src/Lab2_Semana1_Jeremy_Ismael_Trigo/EmailAccount.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab2_Semana1_Jeremy_Ismael_Trigo;
+
+/**
+ *
+ * @author trigo
+ */
+public class EmailAccount {
+    
+}
